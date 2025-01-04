d6a85a7d7daf2680e155cf240e56b9f5735bfa60
Aaron <33428648+AaronVick@users.noreply.github.com>
2025-01-04 12:10:17 -0600

    update

--- api/server/api/index.ts
+++ api/server/api/index.ts
@@ -1,13 +1,19 @@
-import type { Request, Response, NextFunction, Express } from 'express';
+// api/server/api/index.ts
+import { Request, Response, NextFunction, Express } from 'express';
 
 import publicExpressRoutes from './public';
 import teamMemberExpressRoutes from './team-member';
 import teamLeaderApi from './team-leader';
 
-function handleError(err: Error, _: Request, res: Response, __: NextFunction): void {
+const handleError = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+): void => {
   console.error(err.stack);
   res.status(500).json({ error: err.message || 'Internal Server Error' });
-}
+};
 
 export default function api(server: Express): void {
   server.use('/api/v1/public', publicExpressRoutes, handleError);
