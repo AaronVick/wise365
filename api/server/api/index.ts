e834516cbcc3731e072a5fad30a91157e1ac070f
Aaron <33428648+AaronVick@users.noreply.github.com>
2025-01-04 12:04:29 -0600

    update

--- api/server/api/index.ts
+++ api/server/api/index.ts
@@ -1,14 +1,15 @@
-import express from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
+
 import publicExpressRoutes from './public';
 import teamMemberExpressRoutes from './team-member';
 import teamLeaderApi from './team-leader';
 
-function handleError(err: Error, _: any, res: express.Response, __: any) {
+function handleError(err: Error, _: Request, res: Response, __: NextFunction) {
   console.error(err.stack);
   res.json({ error: err.message || err.toString() });
 }
 
-export default function api(server: express.Express) {
+export default function api(server: Express) {
   server.use('/api/v1/public', publicExpressRoutes, handleError);
   server.use('/api/v1/team-member', teamMemberExpressRoutes, handleError);
   server.use('/api/v1/team-leader', teamLeaderApi, handleError);
