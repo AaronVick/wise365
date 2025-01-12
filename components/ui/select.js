acc25277b968943fb13b6b32f7fcccce42a17240
Aaron <33428648+AaronVick@users.noreply.github.com>
2025-01-12 17:23:15 -0500

    update

--- components/ui/select.js
+++ components/ui/select.js
@@ -1,5 +1,9 @@
 // components/ui/select.js
 
+export function Select({ children, className }) {
+  return <div className={`select ${className}`}>{children}</div>;
+}
+
 export function SelectTrigger({ children, className }) {
   return <button className={`select-trigger ${className}`}>{children}</button>;
 }
@@ -12,4 +16,17 @@ export function SelectContent({ children, className }) {
   return <div className={`select-content ${className}`}>{children}</div>;
 }
 
-export default SelectTrigger;
+export function SelectItem({ children, className, onClick }) {
+  return (
+    <div
+      className={`select-item ${className}`}
+      onClick={onClick}
+      role="menuitem"
+      tabIndex={0}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Select;
