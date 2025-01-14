f3000aea426826e383f074f0a7aba20696277d9e
Aaron <33428648+AaronVick@users.noreply.github.com>
2025-01-14 09:41:55 -0500

    update

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
-{
-  "plugins": [
-    ["module-resolver", {
-      "root": ["./"],
-      "alias": {
-        "@": "."
-      }
-    }]
-  ]
-}
+module.exports = {
+  plugins: [
+    ["@babel/plugin-transform-runtime", { regenerator: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+  presets: ["next/babel"],
+};
