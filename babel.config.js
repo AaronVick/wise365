a06379bf7c04d684da120d37d0ed374c0772aaef
Aaron <33428648+AaronVick@users.noreply.github.com>
2025-01-14 09:48:43 -0500

    update

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: [
-    ["@babel/plugin-transform-runtime", { regenerator: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-  ],
-  presets: ["next/babel"],
-};
